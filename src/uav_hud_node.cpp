56ed674dec6410b83419192c03d3483bd83b5b5f
RomanRipp <rippx009@umn.edu>
2016-01-29 18:13:07 -0500

    partial integration with qt

--- src/uav_hud_node.cpp
+++ src/uav_hud_node.cpp
@@ -6,11 +6,22 @@
  */
 
 #include "ros/ros.h"
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtGui/QFont>
+#include <QtGui/QFontDatabase>
 
 #include "ImageConverter.h"
 
 int main(int argc, char **argv) {
   ROS_INFO("Node started");
+
+  QGuiApplication app(argc, argv);
+  QFontDatabase::addApplicationFont(":/fonts/DejaVuSans.ttf");
+  app.setFont(QFont("DejaVu Sans"));
+  QQmlApplicationEngine engine(QUrl("/home/robot/catkin_ws/src/uav_hud/qml/dashboard.qml"));
+  app.exec();
+
   ros::init(argc, argv, "image_converter");
   uav_hud::ImageConverter ic;
   ros::spin();
