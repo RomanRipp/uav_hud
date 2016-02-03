b68f3ac07c692e19bd5251b6a4c16a6e672400a3
RomanRipp <rippx009@umn.edu>
2016-02-03 18:02:43 -0500

    ROS references fixed

--- src/uav_hud_node.cpp
+++ src/uav_hud_node.cpp
@@ -8,21 +8,28 @@
 #include "ros/ros.h"
 #include <QtGui/QGuiApplication>
 #include <QtQml/QQmlApplicationEngine>
-#include <QtGui/QFont>
-#include <QtGui/QFontDatabase>
+#include <QQuickView>
+#include <QQuickItem>
+
 
 #include "ImageConverter.h"
 
 int main(int argc, char **argv) {
-  ROS_INFO("Node started");
+  ROS_INFO("Node starting");
 
-//  QGuiApplication app(argc, argv);
-//  QFontDatabase::addApplicationFont(":/fonts/DejaVuSans.ttf");
-//  app.setFont(QFont("DejaVu Sans"));
-//  QQmlApplicationEngine engine(QUrl("qml/dashboard.qml"));
-//  app.exec();
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/qml/hud.qml")));
 
-  ros::init(argc, argv, "image_converter");
-  uav_hud::ImageConverter ic;
-  ros::spin();
+  QQuickWindow* window = qobject_cast<QQuickWindow*>(engine.rootObjects().at(0));
+  window->show();
+
+  window->setWidth(640);
+  window->setHeight(368);
+
+//  ros::init(argc, argv, "image_converter");
+//  uav_hud::ImageConverter ic;
+//  ros::spin();
+
+  return app.exec();
 }
